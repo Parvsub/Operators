36bc0d07de8c9b601e06ffa95dc9509a68e0c61b
Parvsub <Praveenboda421@gmail.com>
2023-01-31 21:55:46 +0530

    Added = Arrays specific concepts

---
+++ src/com/Array/Smallest.java
@@ -0,0 +1,17 @@
+package com.Array;
+
+public class Smallest {
+    public static void main(String[] args) {
+
+        int arr[] = { 54, 2, 1, 78, 98, 78};
+         int min=arr[0] ;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("smallest: " + min);
+    }
+}
+
+
